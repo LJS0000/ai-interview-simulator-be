4495996f3086f5bd346398c1e4e4bf07e60eee94
LJS0000 <jisu2281l@naver.com>
2023-07-27 11:07:24 +0900

    🎨 IMPRV: User, Conversation 테이블 ForeignKey로 연결

--- gpt/models.py
+++ gpt/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Conversation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     prompt = models.CharField(max_length=512)
     response = models.TextField()
 
